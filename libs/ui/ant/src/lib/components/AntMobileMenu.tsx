caa8b8014290c2088d28b1a1ac562e9cc36ce93d
Lazix95 <lazix95@live.com>
2025-02-26 01:45:27 +0100

    [Ant-UI] Mobile Menu bug fix

--- libs/ui/ant/src/lib/components/AntMobileMenu.tsx
+++ libs/ui/ant/src/lib/components/AntMobileMenu.tsx
@@ -1,8 +1,8 @@
-import { AntMenu, AntMenuItem } from '../elements/AntMenu'
+import { AntMenu, AntMenuItem } from '../elements/AntMenu';
 import { Layout } from 'antd';
 
 export interface AntMobileMenu {
-  items: AntMenuItem[]
+  items: AntMenuItem[];
 }
 
 const footerStyle = {
@@ -10,14 +10,15 @@ const footerStyle = {
   bottom: 0,
   width: '100%',
   display: 'flex',
-  alignItems: 'center'
-} as const
+  alignItems: 'center',
+} as const;
 
-const menuStyles = { flex: 1, justifyContent: 'center', alignItems: 'center', height: '60px' } as const
+const menuStyles = { flex: 1, justifyContent: 'center', alignItems: 'center', height: '60px' } as const;
 
 export function AntMobileMenu({ items }: AntMobileMenu) {
-
-  return <footer style={footerStyle}>
-    <AntMenu menuProps={{ mode: 'horizontal', selectable: false, style: menuStyles }} items={items} />
-  </footer>
+  return (
+    <footer style={footerStyle}>
+      <AntMenu mode={'horizontal'} selectable={false} style={menuStyles} items={items} />
+    </footer>
+  );
 }
